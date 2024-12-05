d4e9407474eed5503f447682431cc9b9ea4fd4cb
Pythonic2023 <daltonfaust7295@gmail.com>
2024-12-05 12:45:31 -0500

    created mapper.py in chap5

--- .venv/lxmlprac.py
+++ .venv/lxmlprac.py
@@ -2,13 +2,14 @@ from io import BytesIO
 from lxml import etree
 import requests
 
-url = 'http://10.0.2.15/'
+url = 'http://192.168.122.76/DVWA'
 r = requests.get(url) # Get request
-content = r.content() # Content type of bytes
+content = r.content # Content type of bytes
 
 parser = etree.HTMLParser()
 content = etree.parse(BytesIO(content), parser=parser) # Parse into a tree
-
-
-for link in content.findall('//a'):
+print('-'*50)
+print(content)
+print('-'*50)
+for link in content.findall('.//a'):
     print(f'{link.get('href')} -> {link.text}')
